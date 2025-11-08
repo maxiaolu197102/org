94fade7088360897543d780b3001ba9c977687ed
maxiaolu1981 <15940995655@139.com>
2025-11-08 15:05:39 +0800

    增加images

---
+++ roam/learn.el
@@ -0,0 +1,25 @@
+;;; learn.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 mxl
+;;
+;; Author: mxl <mxl@mysql>
+;; Maintainer: mxl <mxl@mysql>
+;; Created: November 07, 2025
+;; Modified: November 07, 2025
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex text tools unix vc wp
+;; Homepage: https://github.com/mxl/learn
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'learn)
+;;; learn.el ends here
